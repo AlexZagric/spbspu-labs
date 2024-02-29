6828aefbd9a5332ada0cc74f4bc9605810a02556
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2024-03-01 01:09:57 +0300

    docs: Add documentation for dictionaryList

--- S1/dictionaryList.hpp
+++ S1/dictionaryList.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file DictionaryList.hpp
+ * @brief Шаблон односвязного упорядоченного списка (словаря)
+ * @version 0.1
+ * @date Февраль 2024 г.
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #ifndef DICTIONARY_LIST_HPP
 #define DICTIONARY_LIST_HPP
 #include <cstring>
@@ -5,10 +14,23 @@
 
 namespace zagrivnyy
 {
+  /**
+   * @brief Односвязный упорядоченный список
+   *
+   * Словарь хранит ключ и значение. Ключи не могут повторяться и хранятся строго в лексикографическом порядке. Значения
+   * задаются пользовательским типом.
+   *
+   * @tparam T Тип хранимого значения в словаре
+   */
   template< class T >
   class dictionaryList
   {
   private:
+    /**
+     * @brief Нода словаря
+     *
+     * Хранит в себе ключ, значение и ссылку на следующую ноду. Ключ и значение не могут быть пустыми.
+     */
     struct node_t
     {
       std::string key_ = "";
@@ -29,32 +51,89 @@ namespace zagrivnyy
     dictionaryList(const dictionaryList &src) = delete;
     dictionaryList(dictionaryList &&src);
 
+    /**
+     * @brief Возвращает размер словаря
+     *
+     * @return size_t Количество элементов в словаре
+     */
     size_t size()
     {
       return count_;
     }
 
+    /**
+     * @brief Возвращает первую ноду словаря
+     *
+     * @return node_t* Указатель на первый элемент словаря
+     */
     node_t *start() const
     {
       return head_;
     }
 
+    /**
+     * @brief Возвращает последнюю ноду словаря
+     *
+     * @return node_t* Указатель на последний элемент словаря
+     */
     node_t *end() const
     {
       return tail_;
     }
 
+    /**
+     * @brief Добавить ключ и значение в словарь
+     *
+     * Создает новую ноду используя переданные ключ и значение. Вызывает метод insertNode() для добавления созданной
+     * ноды.
+     *
+     * @see dictionaryList::insertNode
+     *
+     * @param[in] key Ключ словаря, должен быть уникальным значением
+     * @param[in] value Значение сопоставляемое ключу
+     */
     void insert(const std::string &key, const T &value);
+
+    /**
+     * @brief Удалить переданную ноду из словаря
+     *
+     * @throw std::invalid_argument В случае если передан нулевой указатель
+     * @param[in] node Указатель на нужную ноду
+     */
     void erase(const node_t *node);
+
+    // TODO: Write description
     bool find(const std::string &key) const;
 
   private:
-    size_t count_ = 0;
-    node_t *head_ = nullptr;
-    node_t *tail_ = nullptr;
+    size_t count_ = 0;           ///< Количество элементов в словаре
+    node_t *head_ = nullptr;     ///< Указатель на первый элемент словаря
+    node_t *tail_ = nullptr;     ///< Указатель на последний элемент словаря
 
+    /**
+     * @brief Вставляет ноду в список
+     *
+     * Добавляет ноду в нужное место учитывая лексикографическй порядок
+     *
+     * @note Если ключ уже используется, то ничего не произойдет
+     *
+     * @param[in] node Добавляемая нода
+     */
     void insertNode(node_t *node);
+
+    /**
+     * @brief Удаляет ноду из словаря
+     *
+     * @param[in] node Удаляемая нода
+     */
     void deleteNode(node_t *node);
+
+    /**
+     * @brief Поиск ноды по ключу
+     *
+     * @param key Ключ по которому осуществляется поиск
+     * @return node_t* Указатель на найденную ноду
+     */
     node_t *findNode(const std::string &key);
   };
 
