32a5f879a0fec7a4429cfda836bd8d5ae6aac5f7
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2024-04-15 16:41:35 +0300

    feat(S2): Add implementation of StackArray methods

--- S2/StackArray.hpp
+++ S2/StackArray.hpp
@@ -1,6 +1,7 @@
 #ifndef STACKARRAY_HPP
 #define STACKARRAY_HPP
 #include "Stack.hpp"
+#include "StackOverflow.hpp"
 
 namespace zagrivnyy
 {
@@ -8,9 +9,10 @@ namespace zagrivnyy
   class StackArray: public Stack< T >
   {
   public:
-    StackArray(int size = 100);
-    ~Stack() override;
-    void push() override;
+    StackArray(const int size = 100);
+    ~StackArray() override;
+
+    void push(const T &e) override;
     T pop() override;
     bool isEmpty() override;
 
@@ -19,5 +21,53 @@ namespace zagrivnyy
     int top_ = 0;
     int size_ = 0;
   };
+
+  template< class T >
+  StackArray< T >::StackArray(const int size)
+  {
+    try
+    {
+      array_ = new T[size_ = size];
+    }
+    catch (...)
+    {
+      throw T;
+    }
+    top_ = 0;
+  }
+
+  template< class T >
+  StackArray< T >::~StackArray()
+  {
+    delete[] array_;
+  }
+
+  template< class T >
+  void StackArray< T >::push(const T &e)
+  {
+    if (top_ == size_)
+    {
+      throw StackOverflow();
+    }
+
+    array_[++top_] = e;
+  }
+
+  template< class T >
+  T StackArray< T >::pop()
+  {
+    if (top_ == 0)
+    {
+      throw T;
+    }
+
+    return array_[top_--];
+  }
+
+  template< class T >
+  bool StackArray< T >::isEmpty()
+  {
+    return top_ == 0;
+  }
 }
 #endif
